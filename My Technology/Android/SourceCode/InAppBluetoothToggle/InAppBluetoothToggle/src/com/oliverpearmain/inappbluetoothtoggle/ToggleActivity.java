66edd066ef6c5977e0a9c5b5f3a8f64c941f0f4d
Oliver Pearmain <oliverpearmain@gmail.com>
2010-05-15 08:54:08 +0000

    Added v1.0 of Bluetooth Toggle to SVN.
    git-svn-id: file:///Users/okp/Desktop/Dropbox/Code/_SvnRepositories/Android@52 a912d918-a225-7149-8d61-508f829ffe2a

---
+++ My Technology/Android/SourceCode/InAppBluetoothToggle/InAppBluetoothToggle/src/com/oliverpearmain/inappbluetoothtoggle/ToggleActivity.java
@@ -0,0 +1,108 @@
+package com.oliverpearmain.inappbluetoothtoggle;
+
+import android.app.Activity;
+import android.os.Build;
+import android.widget.Toast;
+
+
+public class ToggleActivity extends Activity {
+
+    @Override
+    public void onStart() 
+    {
+    	String debug = "ERROR 001: ";
+    	    	
+    	int deviceApiVersion = 0;
+    	
+    	try
+    	{
+    		deviceApiVersion = Integer.parseInt(Build.VERSION.SDK);
+    	}
+    	catch (Exception e)
+    	{
+        	DisplayToast(debug + "Parsing Build.VERSION.SDK threw exception: " + e.toString());
+    		deviceApiVersion = 0;
+    	}
+    	
+    	try
+    	{
+    		debug = "ERROR 002: ";
+    		    		
+    		if (deviceApiVersion >= 5)
+    		{
+        		debug = "ERROR 200: ";
+        		
+	        	//DisplayToast("Device is version 2.0 or higher");
+    		
+	        	BluetoothToggle2 bt2 = new BluetoothToggle2();
+	        	
+	        	bt2.Toggle(this);
+    		}
+    		else
+    		{
+        		debug = "ERROR 101: ";
+        		
+	        	//DisplayToast("Device is version 1.6 or lower");
+	    		
+	    		BTDevice bluetoothDevice = new BTDevice(getApplicationContext());
+
+	    		debug = "ERROR 102: ";
+	    		
+	    		Boolean isEnabled = bluetoothDevice.isEnabled();
+
+		        String toastErrorMessage = "Bluetooth in Intermediate State";
+		        String toastMessage;
+		        Boolean toggleResult = false;
+		        
+	    		debug = "ERROR 103: ";
+		        toastMessage = "Successfully toggled";
+	
+	    		if (isEnabled)
+	    		{
+	        		debug = "ERROR 104: ";
+	    			toggleResult = bluetoothDevice.toggle();
+		        	toastMessage = "Disabling Bluetooth Connection";
+	    		}
+	    		else
+	    		{
+	        		debug = "ERROR 105: ";
+	    			toggleResult = bluetoothDevice.toggle();
+		        	toastMessage = "Enabling Bluetooth Connection";
+	    		}
+
+        		debug = "ERROR 106: ";
+        		
+		        if (toggleResult)
+		        	DisplayToast(toastMessage);
+		        else 
+		        	DisplayToast(toastErrorMessage);
+    		}
+
+    		debug = "ERROR 107: ";
+    		
+        	super.onStart();
+    	}
+    	catch (Exception e)
+    	{
+    		DisplayToast(debug + e.toString(), 30);    	
+    	}
+    	finally
+    	{
+	        moveTaskToBack(true);
+	        finish();
+    	}
+    	
+    }
+    
+    private void DisplayToast(String message)
+    {
+    	DisplayToast(message, Toast.LENGTH_SHORT);
+    }
+
+    private void DisplayToast(String message, int duration)
+    {
+    	Toast toast = Toast.makeText(this, message, duration);
+    	toast.show();
+    }
+
+}
