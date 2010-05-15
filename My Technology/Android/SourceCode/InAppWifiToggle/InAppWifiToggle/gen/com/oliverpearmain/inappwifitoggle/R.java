f67426eebd7867086cee80edc46b8e4b42f66567
Oliver Pearmain <oliverpearmain@gmail.com>
2010-05-15 09:14:28 +0000

    Fixed gen folder after namespace change.
    git-svn-id: file:///Users/okp/Desktop/Dropbox/Code/_SvnRepositories/Android@56 a912d918-a225-7149-8d61-508f829ffe2a

--- My Technology/Android/SourceCode/InAppWifiToggle/InAppWifiToggle/gen/com/oliverpearmain/inappwifitoggle/R.java
+++ My Technology/Android/SourceCode/InAppWifiToggle/InAppWifiToggle/gen/com/oliverpearmain/inappwifitoggle/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.sas.inappwifitoggle;
+package com.oliverpearmain.inappwifitoggle;
 
 public final class R {
     public static final class attr {
